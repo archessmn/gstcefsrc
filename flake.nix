373a9721a21f37d0b42b349fa55de0f5b72a6791
Mia Moir <me@archess.mn>
2024-12-21 00:04:09 +0000

    feat: The flake now has everything needed to build as documented

--- flake.nix
+++ flake.nix
@@ -22,13 +22,29 @@
             gst_all_1.gstreamer.dev
             gst_all_1.gst-plugins-base.dev
             xorg.libX11.dev
-            xorg.libXrandr.dev
-            xorg.libXcomposite.dev
-            xorg.libXdamage.dev
-            xorg.libXext.dev
-            xorg.libXfixes.dev
-            nss.dev
-            nspr.dev
+          ];
+
+          LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+            alsa-lib
+            at-spi2-core
+            cairo
+            cups
+            dbus
+            expat
+            libdrm
+            libxkbcommon
+            mesa
+            nspr
+            nss
+            pango
+            systemd
+            xorg.libX11
+            xorg.libXcomposite
+            xorg.libXdamage
+            xorg.libXext
+            xorg.libXfixes
+            xorg.libXrandr
+            xorg.libxcb
           ];
         };
       }
